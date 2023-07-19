f81cf98bb91fc14284eb5669828979f4f0894116
Alex Jackson <contact@ajaxbits.com>
2023-07-19 11:39:40 -0500

    chore(audiobookshelf): bump audiobookshelf

--- services/audiobookshelf/arion-compose.nix
+++ services/audiobookshelf/arion-compose.nix
@@ -9,7 +9,7 @@ in {
   services = {
     audiobookshelf.service = {
       container_name = "audiobookshelf";
-      image = "ghcr.io/advplyr/audiobookshelf:2.2.23";
+      image = "ghcr.io/advplyr/audiobookshelf:2.3.2";
       restart = "unless-stopped";
       volumes = [
         "${audiobooksDir}/config:/config"
