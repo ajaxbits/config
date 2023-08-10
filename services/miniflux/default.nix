6a1eac28f709b4dfb1bc68b8340a0cd077368e9e
Alex Jackson <contact@ajaxbits.com>
2023-08-10 18:30:08 -0500

    fix(miniflux): proper syntax
    Dummy

--- services/miniflux/default.nix
+++ services/miniflux/default.nix
@@ -1,7 +1,9 @@
 {
-  enable = true;
-  config = {
-    CLEANUP_FREQUENCY = "48";
-    LISTEN_ADDR = "0.0.0.0:6000";
-  };
+  services.miniflux = {
+    enable = true;
+    config = {
+      CLEANUP_FREQUENCY = "48";
+      LISTEN_ADDR = "0.0.0.0:6000";
+    };
+  }; 
 } 
