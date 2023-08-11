45f5d3ee74808ba6b24866ffb00ab6bc6c8f8a36
Alex Jackson <contact@ajaxbits.com>
2023-08-11 10:04:58 -0500

    fix(miniflux): change miniflux to use a valid port

--- services/miniflux/default.nix
+++ services/miniflux/default.nix
@@ -8,7 +8,7 @@
     adminCredentialsFile = "${config.age.secretsDir}/miniflux/adminCredentialsFile";
     config = {
       CLEANUP_FREQUENCY = "48";
-      LISTEN_ADDR = "0.0.0.0:6000";
+      LISTEN_ADDR = "0.0.0.0:4118";
     };
   };
 
