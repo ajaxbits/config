a51934ff379f184bac192f00057008dbe44ce656
Alex Jackson <contact@ajaxbits.com>
2023-08-11 09:30:13 -0500

    feat(miniflux): inject secrets into miniflux

--- services/miniflux/default.nix
+++ services/miniflux/default.nix
@@ -1,9 +1,23 @@
 {
+  self,
+  config,
+  ...
+}: {
   services.miniflux = {
     enable = true;
+    adminCredentialsFile = "${config.age.secretsDir}/miniflux/adminCredentialsFile";
     config = {
       CLEANUP_FREQUENCY = "48";
       LISTEN_ADDR = "0.0.0.0:6000";
     };
-  }; 
-} 
+  };
+
+  age.secrets = {
+    "miniflux/adminCredentialsFile" = {
+      file = "${self}/secrets/miniflux/adminCredentialsFile.age";
+      mode = "440";
+      owner = config.services.miniflux.user;
+      group = config.services.miniflux.user;
+    };
+  };
+}
