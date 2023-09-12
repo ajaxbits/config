ce13dae4a62e50e459833f82ed37bbd33503a58a
Alex Jackson <contact@ajaxbits.com>
2023-09-12 18:21:53 -0500

    WIP

--- services/unifi/default.nix
+++ services/unifi/default.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  lib = pkgs.lib;
+in {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (pkgs.lib.getName pkg) [
+      "unifi-controller"
+    ];
+
+  services.autoUpgrade.flags = pkgs.lib.mkAfter ["--impure"];
   services.unifi = {
     enable = true;
     unifiPackage = pkgs.unifi7;
     openFirewall = true;
   };
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (pkgs.lib.getName pkg) ["unifi7"];
 }
