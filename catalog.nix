fc04353485672567f2bbdb8a409e0fba98c554cc
Alex Jackson <contact@ajaxbits.com>
2023-09-13 12:44:54 -0500

    feat: init catalog.nix

---
+++ catalog.nix
@@ -0,0 +1,19 @@
+{
+  keys = {
+    aphrodite = "";
+  };
+
+  services = {
+    paperless = {
+      host = "agamemnon";
+    };
+
+    unifi = {
+      host = "agamemnon";
+    };
+
+    jellyfin = {
+      host = "thesprotian";
+    };
+  };
+}
