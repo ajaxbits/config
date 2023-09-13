e4b81af8b957b5317c87737e9b1374b9b592e9c4
Alex Jackson <contact@ajaxbits.com>
2023-09-12 20:40:18 -0500

    unifi

--- services/unifi/default.nix
+++ services/unifi/default.nix
@@ -2,15 +2,12 @@
   config,
   pkgs,
   ...
-}: let
-  lib = pkgs.lib;
-in {
+}: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (pkgs.lib.getName pkg) [
       "unifi-controller"
     ];
 
-  services.autoUpgrade.flags = pkgs.lib.mkAfter ["--impure"];
   services.unifi = {
     enable = true;
     unifiPackage = pkgs.unifi7;
