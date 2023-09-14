1147bdeb8269fa9ae8e163c47ae74dc0f738f350
Alex Jackson <contact@ajaxbits.com>
2023-09-14 13:30:50 -0500

    wip: get agenix working

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
+# TODO: think about agenix-rekey for this
 let
   agamemnon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCc1d8GMX5g84ZFflg8YWJ7rqUlLzVBrc2ENvUqNEGs";
+  temp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILBNlcNWOgrmYIr+imikfZgab1QNzSMWsuR/NgYH1Vwc";
   ajax = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1HH8/qgcU63wichBiB5nvSv0+9B9xxWdy2AYQr3oyr";
   aphrodite = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7omQh72mDWAsnJlXmcNaQOhGKfSj1xpjUVGjAQ5AdB";
 
-  allKeys = [agamemnon ajax aphrodite];
+  allKeys = [agamemnon ajax aphrodite temp];
 in {
   # prometheus
   "prometheus/unpoller-pass.age".publicKeys = allKeys;
