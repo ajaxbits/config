92ce430cbab78e13cac6f947f1d852da9eefcfbf
Alex Jackson <contact@ajaxbits.com>
2023-09-14 13:40:14 -0500

    nix.settings.extra-experimental-featres

--- hosts/temp/configuration.nix
+++ hosts/temp/configuration.nix
@@ -14,14 +14,12 @@
   nix = {
     settings.trusted-users = ["@wheel"];
     settings.auto-optimise-store = true;
+    settings.extra-experimental-features = ["nix-command" "flakes"];
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
   };
 
   # Use the systemd-boot EFI boot loader.
