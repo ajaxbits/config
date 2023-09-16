5f184fba64dc5e359c67a1274a78882dc07a728b
Alex Jackson <contact@ajaxbits.com>
2023-09-16 14:17:36 -0500

    fix: fix inf recursion

--- services/monitoring/default.nix
+++ services/monitoring/default.nix
@@ -3,27 +3,29 @@
   self,
   ...
 }: {
-  imports = [
-    ./uptimekuma.nix
-    ./grafana.nix
-    ./loki.nix
-    (
-      if config.networking.hostName != "patroclus"
-      then
-        import ./prometheus.nix {
-          inherit config;
-          unpollerPass = config.age.secrets."prometheus/unpoller-pass".path;
-        }
-      else {}
-    )
-  ];
+  config = {
+    imports = [
+      ./uptimekuma.nix
+      ./grafana.nix
+      ./loki.nix
+      (
+        if config.networking.hostName != "patroclus"
+        then
+          import ./prometheus.nix {
+            inherit config;
+            unpollerPass = config.age.secrets."prometheus/unpoller-pass".path;
+          }
+        else {}
+      )
+    ];
 
-  age.secrets = {
-    "prometheus/unpoller-pass" = {
-      file = "${self}/secrets/prometheus/unpoller-pass.age";
-      mode = "440";
-      owner = "unpoller-exporter";
-      group = "unpoller-exporter";
+    age.secrets = {
+      "prometheus/unpoller-pass" = {
+        file = "${self}/secrets/prometheus/unpoller-pass.age";
+        mode = "440";
+        owner = "unpoller-exporter";
+        group = "unpoller-exporter";
+      };
     };
   };
 }
