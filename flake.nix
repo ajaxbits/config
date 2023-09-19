03d0aa1c6f81369093fd8612386c171622e30be5
Alex Jackson <contact@ajaxbits.com>
2023-09-19 09:30:02 -0500

    feat: advertiseRoutes on patroclus to see what happens

--- flake.nix
+++ flake.nix
@@ -127,6 +127,7 @@
                 initialAuthKey = "tskey-auth-kHsWhY5CNTRL-ni9hpXJV9W6H9XjaQZRvP6fHiYYmNx7j";
                 tags = ["ajax" "homelab" "nixos"];
                 advertiseExitNode = true;
+                advertiseRoutes = ["172.22.0.0/16"];
               };
             }
           ];
