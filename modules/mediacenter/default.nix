3bbe4678839fe77b88693264724235a6beeda305
Alex Jackson <contact@ajaxbits.com>
2023-09-18 19:57:06 -0500

    fix: add pkgs

--- modules/mediacenter/default.nix
+++ modules/mediacenter/default.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  self,
+  pkgs,
   ...
 }:
 with lib; let
@@ -32,17 +32,17 @@ in {
     };
 
     nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
+      vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    };
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      ];
+    };
   };
 }
