974d0fbd5eb707b7348261f9f642e8dbec573c5f
Alex Jackson <contact@ajaxbits.com>
2023-09-21 19:47:12 -0500

    fix: renamed option

--- modules/audiobookshelf/default.nix
+++ modules/audiobookshelf/default.nix
@@ -125,7 +125,7 @@ in {
     virtualisation.docker.enable = false;
     virtualisation.podman.enable = true;
     virtualisation.podman.dockerSocket.enable = true;
-    virtualisation.podman.defaultNetwork.dnsname.enable = true;
+    virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
     virtualisation.arion.backend = "podman-socket";
   };
 }
