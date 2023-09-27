ba5a1b1cbd9388299ee10e0a331dd0e53c837f0d
Alex Jackson <contact@ajaxbits.com>
2023-09-27 17:59:00 -0500

    feat: add audiobookshelf service

--- modules/audiobookshelf/default.nix
+++ modules/audiobookshelf/default.nix
@@ -2,7 +2,8 @@
   self,
   config,
   lib,
-  inputs,
+  pkgs,
+  unstablePkgs,
   ...
 }: let
   audiobookshelfVersion = "2.4.3";
@@ -32,9 +33,100 @@ in {
       description = "Directory to store mutable config in";
       default = "/data/config";
     };
+    user = mkOption {
+      type = types.str;
+      description = "User to run audiobookshelf as";
+      default = "audiobookshelf";
+    };
+    group = mkOption {
+      type = types.str;
+      description = "Group to run audiobookshelf as";
+      default = "audiobookshelf";
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    systemd.services.audiobookshelf = {
+      description = "Audiobookshelf";
+      after = ["network.target"];
+      wantedBy = ["multi-user.target"];
+      environment = {
+        HOST = cfg.hostname;
+        PORT = toString cfg.port;
+        NODE_ENV = "production";
+        CONFIG_PATH = "${cfg.dataDir}/config";
+        METADATA_PATH = "${cfg.dataDir}/metadata";
+        FFMPEG_PATH = "${pkgs.ffmpeg-full}/bin/ffmpeg";
+        FFPROBE_PATH = "${pkgs.ffmpeg-full}/bin/ffprobe";
+        TONE_PATH = "${pkgs.tone}/bin/tone";
+        SOURCE = "nixpkgs";
+      };
+      path = [
+        pkgs.nodejs_18
+        pkgs.util-linux
+      ];
+      serviceConfig = {
+        WorkingDirectory = lib.mkDefault "/var/lib/audiobookshelf";
+        ExecStart = "exec node ${unstablePkgs.audiobookshelf}/opt/index.js";
+        ExecReload = "kill -HUP $MAINPID";
+        Restart = "always";
+        User = cfg.user;
+        Group = cfg.group;
+        StateDirectory = lib.mkDefault "audiobookshelf";
+        StateDirectoryMode = "0700";
+        ProtectHome = true;
+        ProtectSystem = "strict";
+        PrivateDevices = true;
+        ProtectHostname = true;
+        ProtectClock = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RemoveIPC = true;
+        PrivateMounts = true;
+        Type = "simple";
+        UMask = "0077";
+        TimeoutSec = 15;
+        NoNewPrivileges = true;
+        SystemCallArchitectures = "native";
+        RestrictNamespaces = !config.boot.isContainer;
+        ProtectControlGroups = !config.boot.isContainer;
+        ProtectKernelLogs = !config.boot.isContainer;
+        ProtectKernelModules = !config.boot.isContainer;
+        ProtectKernelTunables = !config.boot.isContainer;
+        LockPersonality = true;
+        PrivateTmp = !config.boot.isContainer;
+        SystemCallFilter = [
+          "~@clock"
+          "~@aio"
+          "~@chown"
+          "~@cpu-emulation"
+          "~@debug"
+          "~@keyring"
+          "~@memlock"
+          "~@module"
+          "~@mount"
+          "~@obsolete"
+          "~@privileged"
+          "~@raw-io"
+          "~@reboot"
+          "~@setuid"
+          "~@swap"
+        ];
+        SystemCallErrorNumber = "EPERM";
+      };
+    };
+    users.users = lib.mkIf (cfg.user != "audiobookshelf") {
+      audiobookshelf = {
+        isSystemUser = true;
+        group = "audiobookshelf";
+        extraGroups = ["mediaoperators"];
+      };
+    };
+    users.groups = lib.mkIf (cfg.group != "audiobookshelf") {
+      audiobookshelf = {};
+      mediaoperators = {};
+    };
+
     virtualisation.oci-containers.backend = "docker";
 
     virtualisation.oci-containers.containers.libation = {
