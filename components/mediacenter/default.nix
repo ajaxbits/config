5959a383fed209ff0d4aa4adccbbf3ca7ff753db
Alex Jackson <contact@ajaxbits.com>
2023-10-06 12:15:28 -0500

    feat: add endpoints for mediacenter

--- components/mediacenter/default.nix
+++ components/mediacenter/default.nix
@@ -107,10 +107,40 @@ in {
       };
     };
 
-    services.caddy.virtualHosts."http://jellyfin.ajax.casa" = lib.mkIf config.components.caddy.enable {
-      extraConfig = ''
-        reverse_proxy http://localhost:8096
-      '';
-    };
+    services.caddy.virtualHosts = let
+      endpoints = [
+        {
+          host = "movies";
+          port = 7878;
+        }
+        {
+          host = "shows";
+          port = 8989;
+        }
+        {
+          host = "subtitles";
+          port = 9898;
+        }
+        {
+          host = "indexers";
+          port = 9696;
+        }
+        {
+          host = "bubflix";
+          port = 8096;
+        }
+        {
+          host = "jellyfin";
+          port = 8096;
+        }
+      ];
+
+      createReverseProxy = attr: {
+        "http://${attr.host}.ajax.casa".extraConfig = ''
+          reverse_proxy http://localhost:${toString attr.port}
+        '';
+      };
+    in
+      builtins.foldl' (a: b: a // b) {} (map createReverseProxy endpoints);
   };
 }
