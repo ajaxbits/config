2c3b6ccbd6d4df86d442e2570b4b6c7db908a972
Alex Jackson <contact@ajaxbits.com>
2023-10-07 19:43:03 -0500

    feat: add targets to smokeping
    fix: typo
    
    fix: add dig binary
    
    fix: proper probe config
    
    fix: typo
    
    fix: indentation
    
    fix: duplicate
    
    fix: remove nextdns probe as test
    
    fix: diaable dns probes
    
    idk anymore
    
    help
    
    arch
    
    :''(
    
    recerting jn working copy aucks
    
    asdf
    
    feat: add smokeping exporter
    
    jjjjj

--- components/monitoring/smokeping.nix
+++ components/monitoring/smokeping.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg.enable = config.components.monitoring.enable && config.components.monitoring.networking.enable;
@@ -8,5 +9,42 @@ in {
   config = lib.mkIf cfg.enable {
     services.smokeping.enable = true;
     services.smokeping.host = "0.0.0.0";
+    services.smokeping.targetConfig = ''
+    
+      probe = FPing
+      menu = Top
+      title = Network Latency Grapher
+      remark = Welcome to the SmokePing website of Arch User. \
+               Here you will learn all about the latency of our network.
+
+      + targets
+      menu = Targets
+      title = Targets
+
+      ++ CloudflareDNS1
+       
+      menu = Cloudflare DNS
+      title = Cloudflare DNS server 1.1.1.1
+      host = 1.1.1.1
+      
+      ++ CloudflareDNS2
+       
+      menu = Cloudflare DNS
+      title = Cloudflare DNS server 1.0.0.1
+      host = 1.0.0.1
+
+      ++ GoogleDNS
+
+      menu = Google DNS
+      title = Google DNS server
+      host = 8.8.8.8
+
+      ++ MultiHost
+
+      menu = Multihost example
+      title = CloudflareDNS and Google DNS
+      host = /targets/CloudflareDNS1 /targets/CloudflareDNS2 /targets/GoogleDNS
+      
+    '';
   };
 }
