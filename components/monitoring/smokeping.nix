9d77862bf8ac707dec7952ea00513f7447a44d07
Alex Jackson <contact@ajaxbits.com>
2023-10-08 08:39:36 -0500

    arch

--- components/monitoring/smokeping.nix
+++ components/monitoring/smokeping.nix
@@ -10,29 +10,34 @@ in {
     services.smokeping.enable = true;
     services.smokeping.host = "0.0.0.0";
     services.smokeping.targetConfig = ''
-      probe = FPing
+    
       menu = Top
       title = Network Latency Grapher
-      remark = Welcome to the SmokePing website of xxx Company. \
+      remark = Welcome to the SmokePing website of Arch User. \
                Here you will learn all about the latency of our network.
-      + Local
-      menu = Local
-      title = Local Network
-      ++ LocalMachine
-      menu = Local Machine
-      title = This host
-      host = localhost
-      + InternetWeather
-      menu = InternetWeather
-      title = Internet Weather
-      ++ Cloudflare
-      menu = Cloudflare
-      title = Cloudflare
+
+      + targets
+      menu = Targets
+      title = Targets
+
+      ++ CloudflareDNS
+       
+      menu = Cloudflare DNS
+      title = Cloudflare DNS server
       host = 1.1.1.1
-      ++ Google
-      menu = Google
-      title = Google
-      host = 8.8.8.8    
+
+      ++ GoogleDNS
+
+      menu = Google DNS
+      title = Google DNS server
+      host = 8.8.8.8
+
+      ++ MultiHost
+
+      menu = Multihost example
+      title = CloudflareDNS and Google DNS
+      host = /targets/CloudflareDNS /targets/GoogleDNS
+      
     '';
   };
 }
