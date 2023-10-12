863d04ef5871181b95d99b5ba750267bccf8c468
Alex Jackson <contact@ajaxbits.com>
2023-10-12 15:38:15 -0500

    Revert "feat: add conditional ha proxy"
    This reverts commit e612e8c4c9dcf1a2a2cac3f7e2cac1485cdbc5d0.

--- components/caddy/default.nix
+++ components/caddy/default.nix
@@ -29,20 +29,5 @@ in {
   config = lib.mkMerge [
     {services.caddy.enable = cfg.enable;}
     monitorConfig
-    {
-      services.caddy.virtualHosts."http://home.ajax.casa" = {
-        extraConfig = ''
-          @home {
-            remote_ip 172.22.0.0/16
-          }
-          @tailscale {
-            remote_ip 100.64.0.0/10
-          }
-          encode gzip zstd
-          reverse_proxy @home http://172.22.0.3:4119
-          reverse_proxy @tailscale http://100.76.177.63:4119
-        '';
-      };
-    }
   ];
 }
