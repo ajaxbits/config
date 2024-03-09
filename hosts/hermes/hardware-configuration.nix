a9589e2a01f5485150b55286c2880b9b537f736c
Alex Jackson <contact@ajaxbits.com>
2024-03-09 14:00:45 -0600

    fix: adjust hardware power for hermes

--- hosts/hermes/hardware-configuration.nix
+++ hosts/hermes/hardware-configuration.nix
@@ -8,30 +8,35 @@
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    inputs.nixos-hardware.nixosModules.dell-xps-15-9560
+    inputs.nixos-hardware.nixosModules.dell-xps-15-9560-intel
   ];
+
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usb_storage"
+      "usbhid"
+      "sd_mod"
+      "rtsx_pci_sdmmc"
+    ];
+    initrd.kernelModules = [];
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "1048576";
+    };
+    kernelParams = [
+      "acpi_rev_override=1"
+      "i915.disable_power_well=0"
+    ];
+    tmp.cleanOnBoot = true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   networking.useDHCP = false;
   networking.interfaces.wlp2s0.useDHCP = true;
-
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usb_storage"
-    "usbhid"
-    "sd_mod"
-    "rtsx_pci_sdmmc"
-  ];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-  boot.kernel.sysctl = {
-    "fs.inotify.max_user_watches" = "1048576";
-  };
-  boot.kernelParams = ["acpi_rev_override=1"];
-  boot.tmp.cleanOnBoot = true;
 
   environment.systemPackages = [
     pkgs.libsmbios
