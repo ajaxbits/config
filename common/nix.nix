c270e6ccc1ff5a6c057c84d05d6f9274652374ad
Alex Jackson <contact@ajaxbits.com>
2024-03-21 19:16:23 -0500

    feat: some nix fixes

--- common/nix.nix
+++ common/nix.nix
@@ -1,8 +1,20 @@
 {
   config.nix = {
-    settings.trusted-users = ["@wheel"];
-    settings.auto-optimise-store = true;
-    settings.extra-experimental-features = ["nix-command" "flakes"];
+    settings = {
+      trusted-users = ["@wheel"];
+      auto-optimise-store = true;
+      extra-experimental-features = [
+        "nix-command"
+        "flakes"
+        "repl-flake"
+        "no-url-literals"
+      ];
+      log-lines = 25;
+      max-free = 3000 * 1014 * 1014;
+      min-free = 512 * 1014 * 1014;
+      builders-use-substitutes = true;
+      connect-timeout = 5;
+    };
     gc = {
       automatic = true;
       dates = "weekly";
