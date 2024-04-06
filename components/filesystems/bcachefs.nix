e2c7f06cea7ca0230232c18295ef1a2f86496a89
Alex Jackson <contact@ajaxbits.com>
2024-04-06 13:06:17 -0500

    [bcache] filesystems: enable upstream bcachefs support (#58)
    filesystems: enable upstream bcachefs support

--- components/filesystems/bcachefs.nix
+++ components/filesystems/bcachefs.nix
@@ -4,15 +4,12 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf mkOverride;
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.components.filesystems.bcachefs;
 in {
   options.components.filesystems.bcachefs.enable = mkEnableOption "Enable bcachefs support";
 
   config = mkIf cfg.enable {
-    # TODO: fix once bcachefs lands in upstream
-    boot.kernelPackages = mkOverride 0 pkgs.linuxPackages_testing_bcachefs.kernel.version;
-
     boot.supportedFilesystems = ["bcachefs"];
     environment.systemPackages = [pkgs.bcachefs-tools];
   };
