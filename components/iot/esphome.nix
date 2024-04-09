9c8a229d93364f3abf4f27a760d25133924f9e81
Alex Jackson <contact@ajaxbits.com>
2024-04-09 09:43:01 -0500

    esphome: 2024.3.1 -> 2024.3.2

--- components/iot/esphome.nix
+++ components/iot/esphome.nix
@@ -10,7 +10,7 @@
     ;
 
   cfg = config.components.iot.esphome;
-  version = "2024.3.1";
+  version = "2024.3.2";
 in {
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers.esphome = {
