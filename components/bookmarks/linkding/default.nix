fc5b6f6f365217e3ec48578be2f336f9d6872bee
Alex Jackson <contact@ajaxbits.com>
2024-04-09 10:08:24 -0500

    feat: enable linkding component

--- components/bookmarks/linkding/default.nix
+++ components/bookmarks/linkding/default.nix
@@ -4,42 +4,30 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf optionalString;
-  cfg = config.components.productivity.todo;
+  cfg = config.components.bookmarks;
   cfgCaddy = config.components.caddy;
 
-  url = "https://todo.${config.networking.domain}";
+  url = "https://links.${config.networking.domain}";
 in {
   options.components.bookmarks = {
     enable = mkEnableOption "Enable bookmark management.";
   };
 
-  config = mkIf cfg.enable {
-    services.vikunja = {
-      enable = true;
-      frontendScheme =
-        if cfgCaddy.enable
-        then "https"
-        else "http";
-      frontendHostname =
-        if cfgCaddy.enable
-        then url
-        else "localhost:${toString config.services.vikunja.port}";
+  config = let
+    port = 9923;
+  in
+    mkIf cfg.enable {
+      imports = [
+        (./compose.nix {
+          inherit port;
+          configDir = "/data/config";
+          public = !cfgCaddy.enable;
+        })
+      ];
+      services.caddy.virtualHosts.${url}.extraConfig = optionalString cfgCaddy.enable ''
+        encode gzip zstd
+        reverse_proxy http://127.0.0.1:${toString port}
+        import cloudflare
+      '';
     };
-    services.caddy.virtualHosts.${url}.extraConfig = optionalString cfgCaddy.enable ''
-      @paths {
-        path /api/* /.well-known/* /dav/*
-      }
-
-      handle @paths {
-        reverse_proxy 127.0.0.1:${toString config.services.vikunja.port}
-      }
-
-      handle {
-        encode zstd gzip
-        root * ${config.services.vikunja.package-frontend}
-        try_files {path} index.html
-        file_server
-      }
-    '';
-  };
 }
