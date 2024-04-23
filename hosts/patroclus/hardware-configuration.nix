d580b80849acdc9df22c3605ba46804b451d7b3f
Alex Jackson <contact@ajaxbits.com>
2024-04-23 13:15:10 -0500

    fix: cpuFreqGovenor multiple defs

--- hosts/patroclus/hardware-configuration.nix
+++ hosts/patroclus/hardware-configuration.nix
@@ -50,6 +50,5 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
