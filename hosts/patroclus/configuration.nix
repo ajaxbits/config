843a00f9d3b880ec9cdb93253b462591b9989c8a
Alex Jackson <contact@ajaxbits.com>
2024-06-13 12:07:35 -0500

    feat: switch over to using invidious component

--- hosts/patroclus/configuration.nix
+++ hosts/patroclus/configuration.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./invidious.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -36,6 +35,7 @@
       enable = true;
       intel.enable = true;
       linux-isos.enable = true;
+      invidious.enable = true;
     };
     miniflux.enable = true;
     monitoring.enable = true;
