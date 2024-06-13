5172f3a0df431c22426062f93fd9c3c02956e00d
Alex Jackson <contact@ajaxbits.com>
2024-06-13 11:08:11 -0500

    temp: add latest hotness

--- hosts/patroclus/invidious.nix
+++ hosts/patroclus/invidious.nix
@@ -1,19 +1,38 @@
-let
-  domain = "https://yt.ajax.casa";
-in {
-  services.invidious = {
-    inherit domain;
-    enable = true;
-    address = "127.0.0.1";
-    port = 3111;
-    settings.db.user = "invidious";
-  };
+{
+  config,
+  pkgsUnstable,
+  ...
+}: {
+  config = {
+    services.invidious = {
+      enable = true;
+      package = pkgsUnstable.invidious;
 
-  services.caddy.virtualHosts."${domain}" = {
-    extraConfig = ''
-      encode gzip zstd
-      reverse_proxy http://127.0.0.1:3111
-      import cloudflare
-    '';
+      domain = "https://yt.ajax.casa";
+      address = "127.0.0.1";
+      port = 3111;
+
+      settings.db.user = "invidious";
+
+      http3-ytproxy = {
+        enable = true;
+        package = pkgsUnstable.http3-ytproxy;
+      };
+    };
+
+    systemd.services.http3-ytproxy.serviceConfig.User = config.services.caddy.user;
+
+    services.caddy.virtualHosts = let
+      inherit (config.services.invidious) address domain port;
+    in {
+      "${domain}".extraConfig = ''
+        encode gzip zstd
+        reverse_proxy http://${address}:${toString port}
+        import cloudflare
+
+        @ytproxy path_regexp ytproxy ^/videoplayback|^/vi/|^/ggpht/|^/sb/
+        reverse_proxy @ytproxy unix//run/http3-ytproxy/socket/http-proxy.sock
+      '';
+    };
   };
 }
