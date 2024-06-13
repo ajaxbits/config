b96ccabdb7ae21608818952d53f3dc4dac7d7d1a
Alex Jackson <contact@ajaxbits.com>
2024-06-13 11:42:41 -0500

    temp: refine proxy configuration

--- hosts/patroclus/invidious.nix
+++ hosts/patroclus/invidious.nix
@@ -12,7 +12,10 @@
       address = "127.0.0.1";
       port = 3111;
 
-      settings.db.user = "invidious";
+      settings = {
+        db.user = "invidious";
+        https_only = true;
+      };
 
       http3-ytproxy = {
         enable = true;
@@ -20,7 +23,10 @@
       };
     };
 
-    systemd.services.http3-ytproxy.serviceConfig.User = config.services.caddy.user;
+    systemd.services.http3-ytproxy = {
+      serviceConfig.User = config.services.caddy.user;
+      environment.DISABLE_WEBP = "1";
+    };
 
     services.caddy.virtualHosts = let
       inherit (config.services.invidious) address domain port;
@@ -31,7 +37,23 @@
         import cloudflare
 
         @ytproxy path_regexp ytproxy ^/videoplayback|^/vi/|^/ggpht/|^/sb/
-        reverse_proxy @ytproxy unix//run/http3-ytproxy/socket/http-proxy.sock
+        reverse_proxy @ytproxy unix//run/http3-ytproxy/socket/http-proxy.sock {
+            header_up X-Forwarded-For ""
+            header_up CF-Connecting-IP ""
+            header_down -alt-svc
+            header_down -Cache-Control
+            header_down -etag
+
+            header_down Cache-Control "private"
+
+            buffer_requests 16MB
+            buffer_responses 16MB
+
+            transport http {
+                versions 1.1
+                keepalive
+            }
+        }
       '';
     };
   };
