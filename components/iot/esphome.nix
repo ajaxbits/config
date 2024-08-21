5a6536ccdbe7631d6527669808f4485a9cb64f20
Alex Jackson <contact@ajaxbits.com>
2024-08-20 22:17:30 -0500

    chore: update esphome

--- components/iot/esphome.nix
+++ components/iot/esphome.nix
@@ -10,7 +10,7 @@
     ;
 
   cfg = config.components.iot.esphome;
-  version = "2024.6.3";
+  version = "2024.7.3";
 in {
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers.esphome = {
