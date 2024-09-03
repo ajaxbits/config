0267f109dd0812edda173779fa3c838042872690
Alex Jackson <contact@ajaxbits.com>
2024-09-03 13:10:42 -0500

    esphome bump

--- components/iot/esphome.nix
+++ components/iot/esphome.nix
@@ -1,37 +1,34 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  inherit
-    (lib)
-    mkIf
-    optionalString
-    ;
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf optionalString;
 
   cfg = config.components.iot.esphome;
-  version = "2024.8.0";
-in {
+  version = "2024.8.3";
+in
+{
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers.esphome = {
       image = "ghcr.io/esphome/esphome:${version}";
       user = "root";
-      volumes = ["/etc/localtime:/etc/localtime:ro" "/data/config/esphome:/config"];
-      extraOptions = ["--network=host"];
+      volumes = [
+        "/etc/localtime:/etc/localtime:ro"
+        "/data/config/esphome:/config"
+      ];
+      extraOptions = [ "--network=host" ];
     };
 
     users.users = {
       esphome = {
         isSystemUser = true;
         group = "esphome";
-        extraGroups = ["configoperators"];
+        extraGroups = [ "configoperators" ];
       };
     };
     users.groups = {
-      esphome = {};
-      configoperators = {};
+      esphome = { };
+      configoperators = { };
     };
-    
+
     services.caddy.virtualHosts."https://esphome.ajax.casa".extraConfig = optionalString config.components.caddy.enable ''
       reverse_proxy http://0.0.0.0:6052
       import cloudflare
