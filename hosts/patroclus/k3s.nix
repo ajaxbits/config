b90d71d1d808f5426a56181cf6232cb3223e1bfb
Alex Jackson <contact@ajaxbits.com>
2025-01-02 17:11:49 -0600

    feat: add openiscsi for longhorn support

--- hosts/patroclus/k3s.nix
+++ hosts/patroclus/k3s.nix
@@ -1,4 +1,9 @@
-{ config, self, ... }:
+{
+  config,
+  self,
+  pkgs,
+  ...
+}:
 {
   services.k3s = {
     enable = true;
@@ -10,6 +15,37 @@
       "--disable traefik"
     ];
   };
+
+  environment.systemPackages =
+    let
+      inherit (pkgs) util-linux kubectl nfs-utils;
+    in
+    [
+      kubectl
+      nfs-utils
+      # longhorn requires nsenter, this package provides it
+      util-linux
+    ];
+
+  # Link nix installed binaries to a path expected by longhorn.
+  # https://github.com/longhorn/longhorn/issues/2166
+  # system.activationScripts.linkBinaries.text = ''
+  #   mkdir -p /usr/local
+  #   if [[ ! -h "/usr/local/bin" ]]; then
+  #     ln -s /run/current-system/sw/bin /usr/local
+  #   fi
+  # '';
+
+  # longhorn looks for nsenter in specific paths, /usr/local/bin is one of
+  # them so symlink the entire system/bin directory there.
+  # https://github.com/longhorn/longhorn/issues/2166#issuecomment-1864656450
+  systemd.tmpfiles.rules = [ "L+ /usr/local/bin - - - - /run/current-system/sw/bin/" ];
+
+  services.openiscsi = {
+    enable = true;
+    name = "${config.networking.hostName}-initiatorhost";
+  };
+
   users = {
     users.k3s = {
       isSystemUser = true;
