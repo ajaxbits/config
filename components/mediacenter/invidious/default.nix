0a1c00d8f969d0250abf72997f3ea5801b8492d4
Alex Jackson <contact@ajaxbits.com>
2025-01-11 11:30:35 -0600

    fix: try disabling http3 yt proxy

--- components/mediacenter/invidious/default.nix
+++ components/mediacenter/invidious/default.nix
@@ -3,22 +3,21 @@
   config,
   pkgsUnstable,
   ...
-}: let
+}:
+let
   inherit (lib) mkIf optionalString;
 
   cfg = config.components.mediacenter.invidious;
   caddyEnabled = config.components.caddy.enable;
-in {
+in
+{
   config = mkIf cfg.enable {
     services.invidious = {
       enable = true;
       package = pkgsUnstable.invidious;
 
       domain = optionalString caddyEnabled "yt.ajax.casa";
-      address =
-        if caddyEnabled
-        then "127.0.0.1"
-        else "0.0.0.0";
+      address = if caddyEnabled then "127.0.0.1" else "0.0.0.0";
       port = 6666;
 
       settings = {
@@ -27,39 +26,19 @@ in {
         external_port = optionalString caddyEnabled 443;
         popular_enabled = false;
       };
-
-      http3-ytproxy = {
-        enable = true;
-        package = pkgsUnstable.http3-ytproxy;
-      };
     };
 
-    systemd.services.http3-ytproxy = {
-      serviceConfig.User = mkIf caddyEnabled config.services.caddy.user;
-      environment.DISABLE_WEBP = "1";
-    };
-
-    services.caddy.virtualHosts = mkIf caddyEnabled (let
-      inherit (config.services.invidious) address domain port;
-    in {
-      "https://${domain}".extraConfig = ''
-        encode gzip zstd
-        reverse_proxy http://${address}:${toString port}
-        import cloudflare
-
-        @ytproxy path_regexp ytproxy ^/videoplayback|^/vi/|^/ggpht/|^/sb/
-        reverse_proxy @ytproxy unix//run/http3-ytproxy/socket/http-proxy.sock {
-            header_up X-Forwarded-For ""
-            header_up CF-Connecting-IP ""
-            header_down -alt-svc
-            header_down -Cache-Control
-            header_down -etag
-            header_down Cache-Control "private"
-            transport http {
-                versions 1.1
-            }
-        }
-      '';
-    });
+    services.caddy.virtualHosts = mkIf caddyEnabled (
+      let
+        inherit (config.services.invidious) address domain port;
+      in
+      {
+        "https://${domain}".extraConfig = ''
+          encode gzip zstd
+          reverse_proxy http://${address}:${toString port}
+          import cloudflare
+        '';
+      }
+    );
   };
 }
