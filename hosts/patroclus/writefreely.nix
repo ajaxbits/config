acd79453cc833b0e1ead4fb587c6ed66f4346601
Alex Jackson <contact@ajaxbits.com>
2025-02-19 19:30:10 -0600

    feat: productionalize writefreely

--- hosts/patroclus/writefreely.nix
+++ hosts/patroclus/writefreely.nix
@@ -1,18 +1,46 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+
+  domain = "posts.ajax.lol";
+in
 {
-  services.writefreely = {
-    enable = true;
-    host = "posts.ajax.lol";
-    admin = {
-      name = "ajaxbits";
-    };
-    settings = {
-      app = {
-        single_user = true;
-        site_description = "Alex's blog.";
-        site_name = "posts";
+  services = {
+    writefreely = {
+      enable = true;
+      host = domain;
+      admin = {
+        name = "ajaxbits";
       };
-      server = {
-        port = 8155;
+      settings = {
+        app = {
+          single_user = true;
+          site_description = "Alex's blog.";
+          site_name = "posts";
+          wf_modesty = true;
+          open_registration = false;
+        };
+        server = {
+          port = 8155;
+        };
+      };
+    };
+    caddy.virtualHosts.${domain} = mkIf config.components.caddy.enable {
+      extraConfig = ''
+        import cloudflare
+        reverse_proxy http://localhost:8155
+        encode gzip zstd
+      '';
+    };
+    cloudflared = mkIf config.components.cloudflared.enable {
+      tunnels."a5466e3c-1170-4a2a-ae62-1a992509f36f".ingress = {
+        ${domain} = {
+          service = "https://localhost:443";
+          originRequest = {
+            originServerName = domain;
+            httpHostHeader = domain;
+          };
+        };
       };
     };
   };
