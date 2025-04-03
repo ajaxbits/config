9e2cb1dcad3edd16d1a08da916f6d64fa407f037
Alex Jackson <contact@ajaxbits.com>
2025-04-03 11:40:54 -0500

    fix: don't keep audit logs forever
    Turns out I was keeping audit logs forever, which made a 600GB audit.log
    file.....
    
    This rotates logs -- 10 files of 100 MB for 1 GB of space

--- common/security.nix
+++ common/security.nix
@@ -1,8 +1,15 @@
 {
+  environment.etc."audit/auditd.conf".text = ''
+    space_left = 10%
+    space_left_action = ignore
+    num_logs = 10
+    max_log_file = 100
+    max_log_file_action = rotate
+  '';
   security = {
     audit = {
       enable = true;
-      rules = ["-a always,exit -F arch=b64 -S execve"];
+      rules = [ "-a always,exit -F arch=b64 -S execve" ];
     };
     auditd.enable = true;
 
