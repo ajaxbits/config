fd3d0394a18e0de7cb601ccbaabc3c50b12c803c
Alex Jackson <contact@ajaxbits.com>
2025-06-24 14:38:51 -0500

    fix: address security patches in lix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     authentik-nix.url = "github:marcelcoding/authentik-nix";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
