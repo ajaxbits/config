a021bee77c6e1bc23356945252ca74feea06439a
Alex Jackson <git@ajaxbits.com>
2025-08-30 16:19:28 -0500

    fix: permissions for ssh key

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ mkdir -p /mnt/etc/ssh /tmp
 BW_SESSION=$(bw login --method 3 --raw || bw unlock --raw)
 export BW_SESSION
 bw get item "Patroclus Private Host Keys" | jq -r .sshKey.privateKey >/mnt/etc/ssh/ssh_host_ed25519_key
+chmod 600 /mnt/etc/ssh/ssh_host_ed25519_key
 
 nom build --accept-flake-config --extra-experimental-features "flakes nix-command" .#nixosConfigurations.patroclusStripped.config.system.build.toplevel
 
