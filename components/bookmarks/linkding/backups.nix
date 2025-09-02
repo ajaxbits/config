480d0fe2c0a043c996417285377e7e3d5dcdbdf7
Alex Jackson <contact@ajaxbits.com>
2025-09-02 13:26:21 -0500

    chore: fmt

--- components/bookmarks/linkding/backups.nix
+++ components/bookmarks/linkding/backups.nix
@@ -17,38 +17,40 @@ in
         backup = pkgs.writeShellScript "linkding-backup" (
           concatLines (
             [ "set -euxo pipefail" ]
-            ++ optional cfg.backups.enable ''
-              BACKUP_TEMP_DIR=$(mktemp -d)
+            ++
+              optional cfg.backups.enable # bash
+                ''
+                  BACKUP_TEMP_DIR=$(mktemp -d)
 
-              ${pkgs.docker}/bin/docker exec ${cfg.linkding.containerName} \
-                  python manage.py \
-                  full_backup /etc/linkding/data/backup.zip
+                  ${pkgs.docker}/bin/docker exec ${cfg.linkding.containerName} \
+                      python manage.py \
+                      full_backup /etc/linkding/data/backup.zip
 
-              ${pkgs.docker}/bin/docker cp \
-                  ${cfg.linkding.containerName}:/etc/linkding/data/backup.zip \
-                  $BACKUP_TEMP_DIR/backup.zip
+                  ${pkgs.docker}/bin/docker cp \
+                      ${cfg.linkding.containerName}:/etc/linkding/data/backup.zip \
+                      $BACKUP_TEMP_DIR/backup.zip
 
-              ${pkgs.rclone}/bin/rclone sync \
-                --config ${rcloneConfigFile} \
-                --verbose \
-                --checksum \
-                --ignore-existing \
-                --transfers=4 \
-                $BACKUP_TEMP_DIR/backup.zip b2-linkding-backups:ajaxbits-linkding-backup/bookmarks
+                  ${pkgs.rclone}/bin/rclone sync \
+                    --config ${rcloneConfigFile} \
+                    --verbose \
+                    --checksum \
+                    --ignore-existing \
+                    --transfers=4 \
+                    $BACKUP_TEMP_DIR/backup.zip b2-linkding-backups:ajaxbits-linkding-backup/bookmarks
 
-              ${pkgs.rclone}/bin/rclone sync \
-                --config ${rcloneConfigFile} \
-                --verbose \
-                --checksum \
-                --ignore-existing \
-                --transfers=4 \
-                $BACKUP_TEMP_DIR/backup.zip r2:linkding-backup
+                  ${pkgs.rclone}/bin/rclone sync \
+                    --config ${rcloneConfigFile} \
+                    --verbose \
+                    --checksum \
+                    --ignore-existing \
+                    --transfers=4 \
+                    $BACKUP_TEMP_DIR/backup.zip r2:linkding-backup
 
-              ${pkgs.docker}/bin/docker exec ${cfg.linkding.containerName} \
-                  rm /etc/linkding/data/backup.zip
+                  ${pkgs.docker}/bin/docker exec ${cfg.linkding.containerName} \
+                      rm /etc/linkding/data/backup.zip
 
-              rm -rfv $BACKUP_TEMP_DIR
-            ''
+                  rm -rfv $BACKUP_TEMP_DIR
+                ''
             ++ optional (
               cfg.backups.healthchecksUrl != ""
             ) "${pkgs.curl}/bin/curl -fsS -m 10 --retry 5 -o /dev/null ${cfg.backups.healthchecksUrl}"
