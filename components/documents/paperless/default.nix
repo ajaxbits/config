af5c172cf768c37c8e389d449fd1c5c2efeb4f2c
Alex Jackson <git@ajaxbits.com>
2025-09-06 11:19:28 -0500

    feat: use the new service option for tika/gotenberg

--- components/documents/paperless/default.nix
+++ components/documents/paperless/default.nix
@@ -22,6 +22,7 @@ in
       user = "paperless";
       mediaDir = dataPaths.documents;
       consumptionDirIsPublic = true;
+      configureTika = true;
 
       address = if config.components.caddy.enable then "127.0.0.1" else "0.0.0.0";
       passwordFile = "${config.age.secretsDir}/paperless/admin-password";
@@ -30,9 +31,6 @@ in
         PAPERLESS_CONSUMER_ASN_BARCODE_PREFIX = "ZB";
         PAPERLESS_CONSUMER_ENABLE_ASN_BARCODE = true;
         PAPERLESS_OCR_USER_ARGS = toJSON { invalidate_digital_signatures = true; };
-        PAPERLESS_TIKA_ENABLED = "1";
-        PAPERLESS_TIKA_ENDPOINT = "http://127.0.0.1:5551";
-        PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:5552";
         PAPERLESS_TIME_ZONE = "America/Chicago";
 
         # This setting can be used to set the three options below (ALLOWED_HOSTS, CORS_ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS). If the other options are set the values will be combined with this one. Do not include a trailing slash. E.g. https://paperless.domain.com Defaults to empty string, leaving the other settings unaffected.
@@ -56,20 +54,6 @@ in
               tls internal
             ''
         );
-    };
-
-    virtualisation.oci-containers.backend = "docker";
-    virtualisation.oci-containers.containers.paperless-tika = {
-      image = "ghcr.io/paperless-ngx/tika:2.9.0-minimal";
-      ports = [ "127.0.0.1:5551:9998" ];
-    };
-    virtualisation.oci-containers.containers.paperless-gotenberg = {
-      image = "docker.io/gotenberg/gotenberg:8.7";
-      ports = [ "127.0.0.1:5552:3000" ];
-      cmd = [
-        "gotenberg"
-        "--chromium-disable-javascript=true"
-      ];
     };
 
     users.users = {
