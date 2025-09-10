73ed16631fdf4d99773925b8b3b70ddc40196289
Alex Jackson <git@ajaxbits.com>
2025-09-10 09:45:00 -0500

    chore: fmt

--- components/audiobookshelf/backup.nix
+++ components/audiobookshelf/backup.nix
@@ -3,46 +3,56 @@
   pkgs,
   lib,
   ...
-}: let
+}:
+let
   inherit (lib) concatLines mkIf optional;
   cfg = config.components.audiobookshelf;
-in {
+in
+{
   config = mkIf cfg.backups.enable {
-    systemd.services.audiobookshelf-backup = let
-      rcloneConfigFile = "${config.age.secretsDir}/rclone/rclone.conf";
+    systemd.services.audiobookshelf-backup =
+      let
+        rcloneConfigFile = "${config.age.secretsDir}/rclone/rclone.conf";
 
-      backup = pkgs.writeShellScript "audiobookshelf-backup" (
-        concatLines (["set -eux"]
-          ++ optional cfg.backups.audiobooks.enable ''
-            ${pkgs.rclone}/bin/rclone sync \
-              --config ${rcloneConfigFile} \
-              --verbose \
-              --checksum \
-              --ignore-existing \
-              --transfers=4 \
-              ${cfg.audiobooksDir} b2-audiobookshelf-backups:ajaxbits-audiobookshelf-backup/audiobooks
-          ''
-          ++ optional cfg.backups.metadata.enable ''
-            ${pkgs.rclone}/bin/rclone sync \
-              --config ${rcloneConfigFile} \
-              --verbose \
-              --checksum \
-              --ignore-existing \
-              --transfers=4 \
-              ${cfg.configDir}/audiobookshelf/metadata/backups b2-audiobookshelf-backups:ajaxbits-audiobookshelf-backup/backups
-          ''
-          ++ optional (cfg.backups.healthchecksUrl != "") "${pkgs.curl}/bin/curl -fsS -m 10 --retry 5 -o /dev/null ${cfg.backups.healthchecksUrl}")
-      );
-    in {
-      script = "${backup}";
+        backup = pkgs.writeShellScript "audiobookshelf-backup" (
+          concatLines (
+            [ "set -eux" ]
+            ++ optional cfg.backups.audiobooks.enable ''
+              ${pkgs.rclone}/bin/rclone sync \
+                --config ${rcloneConfigFile} \
+                --verbose \
+                --checksum \
+                --ignore-existing \
+                --transfers=4 \
+                ${cfg.audiobooksDir} b2-audiobookshelf-backups:ajaxbits-audiobookshelf-backup/audiobooks
+            ''
+            ++ optional cfg.backups.metadata.enable ''
+              ${pkgs.rclone}/bin/rclone sync \
+                --config ${rcloneConfigFile} \
+                --verbose \
+                --checksum \
+                --ignore-existing \
+                --transfers=4 \
+                ${cfg.configDir}/audiobookshelf/metadata/backups b2-audiobookshelf-backups:ajaxbits-audiobookshelf-backup/backups
+            ''
+            ++ optional (
+              cfg.backups.healthchecksUrl != ""
+            ) "${pkgs.curl}/bin/curl -fsS -m 10 --retry 5 -o /dev/null ${cfg.backups.healthchecksUrl}"
+          )
+        );
+      in
+      {
+        script = "${backup}";
 
-      # FIXME: once libation can place files in proper places with permissions, change this
-      serviceConfig = {User = "root";};
-    };
+        # FIXME: once libation can place files in proper places with permissions, change this
+        serviceConfig = {
+          User = "root";
+        };
+      };
 
     systemd.timers.audiobookshelf-backup = {
       description = "Run a audiobookshelf backup on a schedule";
-      wantedBy = ["timers.target"];
+      wantedBy = [ "timers.target" ];
       timerConfig = {
         OnCalendar = "weekly";
         WakeSystem = true;
