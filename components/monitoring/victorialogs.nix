9cce713dd365a1cf7e99491d7214e380589e945a
Alex Jackson <git@ajaxbits.com>
2025-11-22 12:35:20 -0600

    feat: enable journald forwarding to victorialogs

--- components/monitoring/victorialogs.nix
+++ components/monitoring/victorialogs.nix
@@ -1,21 +1,32 @@
 { config, lib, ... }:
 let
   inherit (lib) mkIf;
+  inherit (lib.strings) hasPrefix;
 
   monCfg = config.components.monitoring;
   cfg.enable = monCfg.enable && monCfg.victorialogs.enable;
 in
 {
   config = mkIf cfg.enable {
-    services.victorialogs = {
-      enable = true;
-    };
+    services = {
+      victorialogs.enable = true;
 
-    services.caddy.virtualHosts."https://victorialogs.ajax.casa" = mkIf config.components.caddy.enable {
-      extraConfig = ''
-        reverse_proxy ${config.services.victorialogs.listenAddress}
-        import cloudflare
-      '';
+      journald.upload = {
+        enable = true;
+        settings.Upload.URL =
+          let
+            ensureHost =
+              listenAddress: if hasPrefix ":" listenAddress then "localhost${listenAddress}" else listenAddress;
+          in
+          "http://${ensureHost config.services.victorialogs.listenAddress}/insert/journald";
+      };
+
+      caddy.virtualHosts."https://victorialogs.ajax.casa" = mkIf config.components.caddy.enable {
+        extraConfig = ''
+          reverse_proxy ${config.services.victorialogs.listenAddress}
+          import cloudflare
+        '';
+      };
     };
   };
 }
