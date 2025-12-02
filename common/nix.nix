eb4eb909742774fea052abf1bf2fbb6dfddf4d01
Alex Jackson <git@ajaxbits.com>
2025-12-01 20:08:59 -0600

    feat: move to stable lix in nixpkgs

--- common/nix.nix
+++ common/nix.nix
@@ -1,17 +1,32 @@
+{ pkgs, ... }:
 {
-  config.nix = {
-    settings = {
-      trusted-users = [ "@wheel" ];
-      extra-experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-    optimise.automatic = true;
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+  config = {
+    nixpkgs.overlays = [
+      (final: prev: {
+        inherit (prev.lixPackageSets.stable)
+          colmena
+          nix-eval-jobs
+          nix-fast-build
+          nixpkgs-review
+          ;
+      })
+    ];
+    nix = {
+      package = pkgs.lixPackageSets.stable.lix;
+      settings = {
+        trusted-users = [ "@wheel" ];
+        extra-experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      };
+      optimise.automatic = true;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+
     };
   };
 }
