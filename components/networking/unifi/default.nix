dfdc1143506edf0c28505c1ecfba6dc50f1fa5b6
Alex Jackson <contact@ajaxbits.com>
2026-02-10 10:57:24 -0500

    chore: update unifi

--- components/networking/unifi/default.nix
+++ components/networking/unifi/default.nix
@@ -10,7 +10,7 @@ let
   inherit (builtins) readFile toString;
   cfg = config.components.networking.unifi;
 
-  version = "10.0.162";
+  version = "10.1.84";
 
   dbUser = "unifi";
   dbName = "unifi";
