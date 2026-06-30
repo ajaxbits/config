3b46cc86df1e5982eb212b69fe446d373f61394a
Alex Jackson <contact@ajaxbits.com>
2026-06-30 13:48:29 -0400

    fix: bump caddy plugin sha

--- components/caddy/default.nix
+++ components/caddy/default.nix
@@ -46,7 +46,7 @@ in
       services.caddy = {
         package = pkgs.caddy.withPlugins {
           plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-          hash = "sha256-bL1cpMvDogD/pdVxGA8CAMEXazWpFDBiGBxG83SmXLA=";
+          hash = "sha256-peY/XG37RC0e7FafJ3qNk53srtXZagxN/Hfexcc2TMM=";
         };
         extraConfig = lib.mkBefore ''
           (cloudflare) {
